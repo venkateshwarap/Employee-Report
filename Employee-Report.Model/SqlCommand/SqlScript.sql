fda8ecc14f6c42e5e1d431649736addbc9e74e30
Rajeevp727 <mrrajeev18@gmail.com>
2023-03-09 17:01:08 +0530

    Modified Database.

--- Employee-Report.Model/SqlCommand/SqlScript.sql
+++ Employee-Report.Model/SqlCommand/SqlScript.sql
@@ -1,35 +1,19 @@
 Create database EATracking
 use EATracking
 
---EmployeePOC:
+-----------------------------------------Master-----------------------------------------
 
-CREATE TABLE [dbo].[EmployeePOC](
-    [Id] [int] IDENTITY(1,1) NOT NULL,
-    [POCId] [int] NOT NULL,
+
+CREATE TABLE [dbo].[Employee](
     [EmpId] [nvarchar](10) NOT NULL,
-    [BenchId] [int] NOT NULL,
-    [StartDate] [datetime] NULL,
-    [EndDate] [datetime] NULL,
-    [ReportingTo] [nvarchar](25) NULL,
-    [RoleId] [int] NOT NULL,
-CONSTRAINT [PK_EmployeePOC] PRIMARY KEY CLUSTERED 
+    [Name] [varchar](50) NOT NULL,
+    [Email] [nvarchar](50) NOT NULL,
+CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED 
 (
-    [Id] ASC
+    [EmpId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
-GO ALTER TABLE [dbo].[EmployeePOC]  WITH CHECK ADD  CONSTRAINT [FK_EmployeePOC_POC] FOREIGN KEY([POCId])
-REFERENCES [dbo].[POC] ([Id])
-GO ALTER TABLE [dbo].[EmployeePOC] CHECK CONSTRAINT [FK_EmployeePOC_POC]
-GO ALTER TABLE [dbo].[EmployeePOC]  WITH CHECK ADD  CONSTRAINT [FK_EmployeePOC_Role] FOREIGN KEY([RoleId])
-REFERENCES [dbo].[Role] ([Id])
-GO ALTER TABLE [dbo].[EmployeePOC] CHECK CONSTRAINT [FK_EmployeePOC_Role]
-GO
-
-
-
-
-
-
+ 
 --POC: 
 
 CREATE TABLE [dbo].[POC](
@@ -40,12 +24,6 @@ CONSTRAINT [PK_POC] PRIMARY KEY CLUSTERED 
     [Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
-GO
-
-
-
-
-
 
 --Project:
 
@@ -57,41 +35,6 @@ CONSTRAINT [PK_Project] PRIMARY KEY CLUSTERED 
     [Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
-GO
-
-
-
-
-
-
---EmployeeProject:
-
-CREATE TABLE [dbo].[EmployeeProject](
-    [Id] [int] IDENTITY(1,1) NOT NULL,
-    [ProjectId] [int] NOT NULL,
-    [EmpId] [nvarchar](10) NULL,
-    [StartDate] [datetime] NULL,
-    [EndDate] [datetime] NULL,
-    [ReportingTo] [varchar](50) NULL,
-    [RoleId] [int] NOT NULL,
-    [Achivements] [varchar](200) NULL,
-CONSTRAINT [PK_EmployeeProject] PRIMARY KEY CLUSTERED 
-(
-    [Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO ALTER TABLE [dbo].[EmployeeProject]  WITH CHECK ADD  CONSTRAINT [FK_EmployeeProject_Project] FOREIGN KEY([ProjectId])
-REFERENCES [dbo].[Project] ([Id])
-GO ALTER TABLE [dbo].[EmployeeProject] CHECK CONSTRAINT [FK_EmployeeProject_Project]
-GO ALTER TABLE [dbo].[EmployeeProject]  WITH CHECK ADD  CONSTRAINT [FK_EmployeeProject_Role] FOREIGN KEY([RoleId])
-REFERENCES [dbo].[Role] ([Id])
-GO ALTER TABLE [dbo].[EmployeeProject] CHECK CONSTRAINT [FK_EmployeeProject_Role]
-GO
-
-
-
-
-
 
 --Role
 
@@ -103,12 +46,6 @@ CONSTRAINT [PK_Role] PRIMARY KEY CLUSTERED 
     [Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
-GO
-
-
-
-
-
 
 --Skills
 
@@ -120,12 +57,6 @@ CREATE TABLE [dbo].[Skills](
 	[ID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-
-
-
-
-
 
 --Interview
 
@@ -135,36 +66,13 @@ CREATE TABLE [dbo].[Interviews](
 	[Skill][nvarchar](100) NOT NULL,
 	[Role] [nvarchar](50) NOT NULL,
 	[Status] [nvarchar](10) NOT NULL,
-	[Date] [datetime] NOT NULL,
+	[Date] [date] NOT NULL,
 	[ReportingTo] [nvarchar](100) NOT NULL,
  CONSTRAINT [PK_Interviews] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
-GO
-
-
-
-
-
-
---EACouncilEntryExit
-
-CREATE TABLE [dbo].[EACouncilEntryExit](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[EmpId] [varchar](10) NULL,
-	[StartDate] [datetime] NULL,
-	[EndDate] [datetime] NULL,
-	[Role] [nvarchar](50) NULL,
-	[ReportingTo] [varchar](10) NULL
-) ON [PRIMARY]
-GO
-
-
-
-
-
 
 --Certifications
 
@@ -172,47 +80,21 @@ CREATE TABLE [dbo].[Certifications](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[EmpId] [varchar](10) NULL,
 	[Name] [varchar](250) NULL,
-	[ValidFrom] [datetime] NULL,
-	[ValidTill] [datetime] NULL,
+	[ValidFrom] [date] NULL,
+	[ValidTill] [date] NULL,
 	[EACId] [int] NULL
 ) ON [PRIMARY]
-GO
 
+--Learnings
 
-
-
-
-
---LEARNINGS
-
-CREATE TABLE LEARNINGS
+CREATE TABLE Learnings
 (ID INT PRIMARY KEY IDENTITY(1,1),
 SkillID int,
 HoursOfLearning int,
 Name VARCHAR(30),
 Path VARCHAR(500),
-StartDate datetime,
-EndDate datetime )
-
-
-
-
-
-
---EmployeeLearning
-
-CREATE TABLE EmployeeLearning
-(ID INT PRIMARY KEY IDENTITY(1,1),
-LearningID INT,
-EmpID INT,
-StartDate DATETIME,
-EndDate DATETIME,
-BenchID INT)
-
-
-
-
-
+StartDate date,
+EndDate date )
 
 --Training
 
@@ -220,14 +102,87 @@ CREATE TABLE  Training
 (ID INT PRIMARY KEY IDENTITY(1,1),
 Name VARCHAR (30),
 HoursOfLearning int,
-StartDate datetime,
-EndDate datetime
+StartDate date,
+EndDate date
 )
 
+-----------------------------------------Employee-----------------------------------------
+
+--EmployeePOC:
+
+CREATE TABLE [dbo].[EmployeePOC](
+    [Id] [int] IDENTITY(1,1) NOT NULL,
+    [POCId] [int] NOT NULL,
+    [EmpId] [nvarchar](10) NOT NULL,
+    [BenchId] [int] NOT NULL,
+    [StartDate] [date] NULL,
+    [EndDate] [date] NULL,
+    [ReportingTo] [nvarchar](25) NULL,
+    [RoleId] [int] NOT NULL,
+CONSTRAINT [PK_EmployeePOC] PRIMARY KEY CLUSTERED 
+(
+    [Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+  ALTER TABLE [dbo].[EmployeePOC]  WITH CHECK ADD  CONSTRAINT [FK_EmployeePOC_POC] FOREIGN KEY([POCId])
+REFERENCES [dbo].[POC] ([Id])
+  ALTER TABLE [dbo].[EmployeePOC] CHECK CONSTRAINT [FK_EmployeePOC_POC]
+  ALTER TABLE [dbo].[EmployeePOC]  WITH CHECK ADD  CONSTRAINT [FK_EmployeePOC_Role] FOREIGN KEY([RoleId])
+REFERENCES [dbo].[Role] ([Id])
+ALTER TABLE [dbo].[EmployeePOC] CHECK CONSTRAINT [FK_EmployeePOC_Role]
+
+ALTER TABLE [dbo].[EmployeePOC]  WITH CHECK ADD  CONSTRAINT [FK_EmployeePOC_Employee] FOREIGN KEY([EmpId])
+REFERENCES [dbo].[Employee] ([EmpId])
+ALTER TABLE [dbo].[EmployeePOC] CHECK CONSTRAINT [FK_EmployeePOC_Employee]
 
 
+--EmployeeProject:
 
+CREATE TABLE [dbo].[EmployeeProject](
+    [Id] [int] IDENTITY(1,1) NOT NULL,
+    [ProjectId] [int] NOT NULL,
+    [EmpId] [nvarchar](10) NULL,
+    [StartDate] [date] NULL,
+    [EndDate] [date] NULL,
+    [ReportingTo] [varchar](50) NULL,
+    [RoleId] [int] NOT NULL,
+    [Achivements] [varchar](200) NULL,
+CONSTRAINT [PK_EmployeeProject] PRIMARY KEY CLUSTERED 
+(
+    [Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+ALTER TABLE [dbo].[EmployeeProject]  WITH CHECK ADD  CONSTRAINT [FK_EmployeeProject_Project] FOREIGN KEY([ProjectId])
+REFERENCES [dbo].[Project] ([Id])
+ALTER TABLE [dbo].[EmployeeProject] CHECK CONSTRAINT [FK_EmployeeProject_Project]
+ALTER TABLE [dbo].[EmployeeProject]  WITH CHECK ADD  CONSTRAINT [FK_EmployeeProject_Role] FOREIGN KEY([RoleId])
+REFERENCES [dbo].[Role] ([Id])
+ALTER TABLE [dbo].[EmployeeProject] CHECK CONSTRAINT [FK_EmployeeProject_Role]
 
+ALTER TABLE [dbo].[EmployeeProject]  WITH CHECK ADD  CONSTRAINT [FK_EmployeeProject_Employee] FOREIGN KEY([EmpId])
+REFERENCES [dbo].[Employee] ([EmpId])
+ALTER TABLE [dbo].[EmployeeProject] CHECK CONSTRAINT [FK_EmployeeProject_Employee]
+ 
+--EACouncilEntryExit
+
+CREATE TABLE [dbo].[EACouncilEntryExit](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[EmpId] [varchar](10) NULL,
+	[StartDate] [date] NULL,
+	[EndDate] [date] NULL,
+	[Role] [nvarchar](50) NULL,
+	[ReportingTo] [varchar](10) NULL
+) ON [PRIMARY]
+ 
+--EmployeeLearning
+
+CREATE TABLE EmployeeLearning
+(ID INT PRIMARY KEY IDENTITY(1,1),
+LearningID INT,
+EmpID INT,
+StartDate date,
+EndDate date,
+BenchID INT)
 
 --EmployeeTraining
 
@@ -235,7 +190,6 @@ CREATE TABLE EmployeeTraining
 (ID INT PRIMARY KEY IDENTITY(1,1),
 EmpID INT,
 TraningID INT,
-StartDate DATETIME,
-EndDate DATETIME,
+StartDate date,
+EndDate date,
 BenchID INT)
-
