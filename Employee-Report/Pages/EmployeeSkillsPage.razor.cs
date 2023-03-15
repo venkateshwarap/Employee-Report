18f6df24c755620266565725c9fae2b432390c5c
prafulkumarreddy <praful.reddy@motivitylabs.com>
2023-03-15 17:31:34 +0530

    Implemented Dropdown in EmployeeSkill

--- Employee-Report/Pages/EmployeeSkillsPage.razor.cs
+++ Employee-Report/Pages/EmployeeSkillsPage.razor.cs
@@ -1,6 +1,8 @@
 ﻿using Employee.DataModel.Models;
 using Employee_Report.API.Entities;
+using Employee_Report.API.Service;
 using Employee_Report.Model.Models;
+using Employee_Report.Utilities;
 
 namespace Employee_Report.Pages
 {
@@ -10,10 +12,18 @@ namespace Employee_Report.Pages
         public IEnumerable<EmployeeSkills_Skills_Entity> employeeSkills_SkillsDetails { get; set; }
         public EmployeeSkills_Skills_Entity EmployeeSkills_SkillsModel = new EmployeeSkills_Skills_Entity();
         public EmployeeSkills empSkillsModel = new EmployeeSkills();
+          Repository.Services.SkillsService SkillsService = new();
         private bool IsHidden { get; set; } = false;
+
+        List<Skill> skillDetails = new List<Skill>();
+        public Skill skillModel = new();
         protected override async Task OnInitializedAsync()
         {
             employeeSkills_SkillsDetails = (await employeeSkillService.GetEmployeeSkills_Skills()).ToList();
+            var response = await SkillsService.GetSkills();
+            skillDetails = Utility.GetResponseData<List<Skill>>(response.response);
+
+
         }
         public async void AddEmployeeSkill()
         {
